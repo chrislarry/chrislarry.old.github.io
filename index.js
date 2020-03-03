343e0d6cd199d5427821dbbd4f954b960c391ca3
chrislarry <47925529+chrislarry@users.noreply.github.com>
2020-03-03 16:47:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ var fi = prompt("το ποσοστό των πλανητών στους οποί
 var fc = prompt(" το ποσοστό των πολιτισμών αυτών οι οποίοι έχουν τις τεχνικές δυνατότητες απομακρυσμένης επικοινωνίας");
 var L = prompt("η χρονική διάρκεια κατά την οποία αυτοί οι πολιτισμοί εκπέμπουν ανιχνεύσιμα σήματα.");
 var N = R * fp * ne * fl * fi * fc * L
-alert("Σύμφωνα με τα στοιχεία που έδωσες, " + N + " εξωγήινοι πολιτισμοί υπάρχουν στο γαλαξία μας.  ");
+document.getElementById("aliens").innerHTML = "Σύμφωνα με τα στοιχεία που έδωσες, " + N + " εξωγήινοι πολιτισμοί υπάρχουν στο γαλαξία μας.  ";
 }
 
-
-function myip() {
+function myip(){
 var findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}})
 /*Usage example*/
-findIP.then(ip => alert('your ip: ' + ip)).catch(e => console.error(e))
-
+findIP.then(ip => document.getElementById("addrr").innerHTML = ip).catch(e => console.error(e))
 }
- 
+
 
