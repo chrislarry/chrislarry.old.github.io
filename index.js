5c8f55ec11a17d2f1c259034376a7fe35846de0d
chrislarry <47925529+chrislarry@users.noreply.github.com>
2020-03-04 14:11:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var fi = prompt("το ποσοστό των πλανητών στους οποί
 var fc = prompt(" το ποσοστό των πολιτισμών αυτών οι οποίοι έχουν τις τεχνικές δυνατότητες απομακρυσμένης επικοινωνίας");
 var L = prompt("η χρονική διάρκεια κατά την οποία αυτοί οι πολιτισμοί εκπέμπουν ανιχνεύσιμα σήματα.");
 var N = R * fp * ne * fl * fi * fc * L
-document.getElementById("aliens").innerHTML = "Σύμφωνα με τα στοιχεία που έδωσες, " + N + " εξωγήινοι πολιτισμοί υπάρχουν στο γαλαξία μας.  ";
+document.getElementById("aliens").innerHTML = "Σύμφωνα με τα στοιχεία που έδωσες " + name + ", " + N + " εξωγήινοι πολιτισμοί υπάρχουν στο γαλαξία μας.  ";
 }
 
 function myip(){
@@ -17,4 +17,30 @@ var findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPe
 findIP.then(ip => document.getElementById("addrr").innerHTML = ip).catch(e => console.error(e))
 }
 
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
 
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
