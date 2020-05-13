912d79b10dfc82e4064a764d56d4ec03cc7496d4
chrislarry <47925529+chrislarry@users.noreply.github.com>
2020-05-13 03:09:58 +0300

    Add files via upload

--- millionaire/main.js
+++ millionaire/main.js
@@ -12,6 +12,7 @@ let chearsnd = new Audio('snd/chearing.mp3');
 let boosnd = new Audio('snd/boo.mp3');
 let endsnd = new Audio('snd/end.mp3');
 let defaultcolor='#191f46';
+let lifes = 2 , highscore=0;
 
 function onload(){
     //getpoint();
@@ -34,28 +35,31 @@ function intro(){
 title.innerHTML=`<center><br>
     Ποιος θέλει να γίνει εκατομμυριούχος<br>
     <img src="Millionaire.png" width="180px"></center><br>
-    <p>Καλός ήρθες στο ποιος θέλει να γίνει εκατομμυριούχος.<br>
+    <p>Καλώς ήρθες στο ποιος θέλει να γίνει εκατομμυριούχος.<br>
     Έχεις 60 δευτερόλεπτα να βρεις την απάντηση.<br>
     Το παιχνίδι ξεκινάει σύντομα.</p>
 `;
-setTimeout(Startgame,17000)
+setTimeout(Startgame,2000)
 }
 
 function timer(){
-    time--;
+    
     if (time >=30){
-        document.getElementById('timer').innerHTML=`<div><h3 style="color:green;">${time}</h1><div>`;
+        document.getElementById('timer').innerHTML=`<h3 style="color:green;">${time}</h3>`;
     } else if (time<30 && time >10){
-        document.getElementById('timer').innerHTML=`<div><h3 style="color:yellow">${time}</h1><div>`;
+        document.getElementById('timer').innerHTML=`<h3 style="color:yellow">${time}</h3>`;
+    } else if (time <= 0){ 
+        checkanswer();
     } else {
-        document.getElementById('timer').innerHTML=`<div><h3 style="color:red">${time}</h1><div>`;
+        document.getElementById('timer').innerHTML=`<h3 style="color:red">${time}</h3>`;
     }
-    if (time <= 0){ checkanswer(); }
+    
+    time--;
 }
 
-function getpoint(){
-    document.getElementById('points').innerHTML='Points: '+points;
-}
+//function getpoint(){
+//    document.getElementById('points').innerHTML=`Έχεις ${points} €`;
+//}
 function countsong(){
     countsnd.load();
     countsnd.play();
@@ -72,18 +76,18 @@ function Startgame(){
     questionsnd.play();
  setTimeout(countsong,2000);
     timers = setInterval(timer ,1000);
-    i=Math.floor((Math.random() * 7) + 1);
-    getpoint();
+    i=Math.floor((Math.random() * 8) + 1);
+ //   getpoint();
     fetch("questions.json")
                 .then(response => response.json())
                 .then(data => {
              this.data=data[i];       
              console.log(this.data);
-    console.log(points);
+    console.log(data);
 
     title.innerHTML='';
     question.innerHTML=`
-   <center> <h2>Έχεις ${points} €</h2><img src="Millionaire.png" width="80px"><br>
+   <center> <h2>Έχεις ${points} € και ${lifes} ζωές. Ερώτηση ${qno}</h2><img src="Millionaire.png" width="80px"><br>
     <div id="timer" style="fontsize:40px; margin-top:10px">60</div>
     </center>
        <div  class="question">${this.data.question}</div> `;
@@ -113,7 +117,6 @@ function checkanswer(clr){
     clearInterval(timers);
     time=60;
     if ( clr == this.data.correctanswer){
-
         chearsnd.load();
         chearsnd.play();
         qno++;
@@ -126,16 +129,14 @@ function checkanswer(clr){
         question.innerHTML=`
    <center><h2>Έχεις ${points}€. Ερώτηση νούμερο ${qno} για ${foreuro}€</h2>
      <img src="Millionaire.png" width="140px"><br><br><br></center>
-     <div  class="question">${this.data.question}</div> 
-   
-        `;
+     <div  class="question">${this.data.question}</div>    `;
         setTimeout(Startgame,5000);
-    } else {
 
- 
+    } else {
+    
+
         boosnd.load();
         boosnd.play()
-        setTimeout(endsong,1500);
         
         if (clr == 'a'){ theanswera = 'red'}
         else if (clr == 'b'){ theanswerb = 'red'}
@@ -148,12 +149,11 @@ function checkanswer(clr){
     question.innerHTML=`
    <center><h2> Η σωστή απάντηση είναι το ${this.data.correctanswer}. Έφτασες τα ${points}€</h2>
     <img src="Millionaire.png" width="100px"><br><br></center>
-    <button onclick="location.reload();">Νέο παιχνίδι</button>
+    
      <div  class="question">${this.data.question}</div> `;
         this.data.correctanswer = '';
-        points=0;
+        
     }
-
     answers.innerHTML=`<div class="row"><div class="col">
         <button style="background:${theanswera}">Α. ${this.data.a}</button>
         <button  style="background:${theanswerc}">Γ. ${this.data.c}</button>
@@ -162,14 +162,47 @@ function checkanswer(clr){
         <button  style="background:${theanswerd}">Δ. ${this.data.d}</button>
             </div></div>`;
         console.log(this.data.correctanswer);
+        if (lifes >0 && this.data.correctanswer != clr) {
+            lifes--;
+            setTimeout(Startgame ,5000);
+        } else if (lifes <=0 && this.data.correctanswer != clr){
+            
+            question.innerHTML='';
+            answers.innerHTML='';
+            title.innerHTML=`
+            Έφτασες τα ${points} ευρώ<br> με ${qno} σωστές απαντήσεις 
+            `;
+            highscoref();
+
+            setTimeout(endsong,1500);
+            setTimeout(endgame,5000);     
+
+            
+}
 }
 
-function getQueryVariable(variable){
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
+function highscoref(){
+   highscore = localStorage.getItem('highscore');
+   if (qno>highscore){
+       question.innerHTML='<h2>you have a new high score ' +qno+'<br> Your old highscore was'+highscore+'</h2>';
+       localStorage.setItem('highscore',qno);
+
+    } else {
+        question.innerHTML=`<h2>your score is ${qno} <br>your high score is ${highscore}</h2>`;
+
+   }
+}
+
+function endgame(){
+    highscoref();
+    title.innerHTML=`<h1>Millionaire</h1>
+    <img src="Millionaire.png" width="140px"><br><br>
+    <p>Copyright by Chris Larry<br><br>
+    https://chrislarry.github.io<br><br>
+    chris_larry@riseup.net<br><br>
+    <button onclick="location.reload();">Νέο παιχνίδι</button>
+
+    `;
+ //   question.innerHTML='';
+    answers.innerHTML='';
 }
